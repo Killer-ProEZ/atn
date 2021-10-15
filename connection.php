6e6f45f784d893d802f52bf2f21a64c9e2c67b99
Killer-ProEZ <ht01646@gmail.com>
2021-10-15 17:07:59 +0700

    v0.3

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$conn=pg_connect("postgres://kdnderyiaekdim:48dc0ac3a04fb186a060a46cb004026b3b5e6b70d7d3d2e1e22502455ff40ec0@ec2-54-147-76-191.compute-1.amazonaws.com:5432/dfhbpnfbi8n7kg");
+$conn = pg_connect("postgres://lvwhzzxywakucf:4f7a868361388ac187f4cbb59b9b63e680694aafcf93a4275d3215d82249fb07@ec2-34-203-91-150.compute-1.amazonaws.com:5432/d81lboqi7rhl24");
 // $conn=mysqli_connect('localhost','root','','laptop')
 // or die("Can not connect database".mysqli_connect_error());
 // $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=19091995");
-if(!$conn){
+if (!$conn) {
     die("Connection failed");
 }
