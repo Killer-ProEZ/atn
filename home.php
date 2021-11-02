4c6ef9f5ec4e3436c84ae5b97a59c6039ed88f38
Killer-ProEZ <ht01646@gmail.com>
2021-11-02 19:52:30 +0700

    v0.9

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
  <!-- ======= Hero Section ======= -->
- <section id="hero" class="d-flex align-items-center" style="background-image: url('assets/img/background.jpg');">
+ <section id="hero" class="d-flex align-items-center" style="background-image: url('./assets/img/background.jpg');">
    <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
      <h1>Incredible Prices on All Your Favorite Items</h1>
      <h2>Design is thinking made visual</h2>
