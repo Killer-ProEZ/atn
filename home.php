6fde45b11999b6b9097cd547f375f37c86294107
Killer-ProEZ <ht01646@gmail.com>
2021-11-02 22:15:38 +0700

    v0.9

--- home.php
+++ home.php
@@ -39,28 +39,19 @@
    </section><!-- End Clients Section -->
    <!-- ======= About Section ======= -->
    <section id="about" class="about">
-   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="assets/img/hostdeal1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="assets/img/hostdeal2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="assets/img/hostdeal3.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-</section>
+     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+       <div class="carousel-inner">
+         <div class="carousel-item">
+           <img src="assets/img/hostdeal1.jpg" class="d-block w-100" alt="...">
+         </div>
+         <div class="carousel-item">
+           <img src="assets/img/hostdeal2.jpg" class="d-block w-100" alt="...">
+         </div>
+         <div class="carousel-item">
+           <img src="assets/img/hostdeal3.jpg" class="d-block w-100" alt="...">
+         </div>
+       </div>
+     </div>
      <section id="portfolio" class="portfolio">
        <div class="container">
          <div class="section-title" data-aos="fade-left">
