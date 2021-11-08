8608e7db04f9d5fd90d30721a53462a4ecf920a6
Killer-ProEZ <ht01646@gmail.com>
2021-11-08 22:43:19 +0700

    v0.9

--- report.php
+++ report.php
@@ -54,8 +54,6 @@ if (isset($_POST["btn_summit"])) {
     } else if (trim($endday) == "") {
         echo "<script type='text/javascript'>alert('EndDay can not be empty');</script>";
     }
-    echo $startday;
-    echo $endday;
     echo '<meta http-equiv="refresh" content="0;URL=?page=report&&startday=' . $startday . '&&endday=' . $endday . '">';
 }
 ?>
